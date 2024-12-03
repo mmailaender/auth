084e5fc192d7a9eed3f21b817d90d69cc95a328b
Micha Mailaender <micha@mailaender.me>
2024-12-03 17:22:09 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<h1>Welcome to SvelteKit</h1>
+<button class="btn preset-filled-primary-500">Hello</button>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+import type { Config } from 'tailwindcss';
+
+import { skeleton, contentPath } from '@skeletonlabs/skeleton/plugin';
+import * as themes from '@skeletonlabs/skeleton/themes';
+
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}', contentPath(import.meta.url, 'svelte')],
+
+	theme: {
+		extend: {}
+	},
+
+	plugins: [
+		skeleton({
+			// NOTE: each theme included will be added to your CSS bundle
+			themes: [themes.cerberus, themes.rose]
+		}),
+		typography,
+		forms
+	]
+} satisfies Config;
