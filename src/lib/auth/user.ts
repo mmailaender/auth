d41c6055f2bbd95963b4f8359fe232e5cc7e0143
Micha Mailaender <micha@mailaender.me>
2024-12-14 21:41:33 +0100

    Add logging

--- src/lib/auth/user.ts
+++ src/lib/auth/user.ts
@@ -20,48 +20,58 @@ export async function signUpWithPasskey(
 	lastName: string,
 	email: string
 ): Promise<Tokens> {
-	const query = fql`signUpWithPasskey(${credential}, { firstName: ${firstName}, lastName: ${lastName}, email: ${email} })`
-	console.log("\nuser.ts \n", "signUpWithPasskey query: ", query.encode());
-	
+	const query = fql`signUpWithPasskey(${credential}, { firstName: ${firstName}, lastName: ${lastName}, email: ${email} })`;
+	// console.log('\nuser.ts \n', 'signUpWithPasskey query: ', query.encode());
+	console.log(
+		'\nuser.ts \n',
+		'signUpWithPasskey query: ',
+		`signUpWithPasskey({${credential.id}, ${credential.userId}, ${credential.algorithmId}, ${credential.publicKey}, { firstName: ${firstName}, lastName: ${lastName}, email: ${email} })`
+	);
+
 	const response = await sClient.query<Tokens>(query);
 
 	return response.data;
 }
 
-export async function signInWithSocialProvider(provider: Provider, providerAccountId: string): Promise<Tokens> {
+export async function signInWithSocialProvider(
+	provider: Provider,
+	providerAccountId: string
+): Promise<Tokens> {
 	const response = await sClient.query<Tokens>(
 		fql`signInWithSocialProvider(${provider}, ${providerAccountId})`
 	);
 
-	return response.data
+	return response.data;
 }
 
-
 export async function signInWithPasskey(providerAccountId: string): Promise<Tokens> {
-	const response = await sClient.query<Tokens>(
-		fql`signInWithPassKey(${providerAccountId})`
-	);
+	const response = await sClient.query<Tokens>(fql`signInWithPassKey(${providerAccountId})`);
 
-	return response.data
+	return response.data;
 }
 
 /**
- * 
+ *
  * @returns Returns the current user based on the access token
  */
 export async function getUser(accessToken: string): Promise<User> {
 	const response = await uClient(accessToken).query<User>(fql`Query.identity()`);
-	return response.data
+	return response.data;
 }
 
 export async function verifyUserExists(email: string): Promise<boolean> {
 	const response = await sClient.query<boolean>(fql`verifyUserExists(${email})`);
-	return response.data
+	return response.data;
 }
 
-export async function verifySocialAccountExists(provider: Provider, providerAccountId: string): Promise<boolean> {
-	const response = await sClient.query<boolean>(fql`verifySocialAccountExists(${provider}, ${providerAccountId})`);
-	return response.data
+export async function verifySocialAccountExists(
+	provider: Provider,
+	providerAccountId: string
+): Promise<boolean> {
+	const response = await sClient.query<boolean>(
+		fql`verifySocialAccountExists(${provider}, ${providerAccountId})`
+	);
+	return response.data;
 }
 
 export type User = Document & {
