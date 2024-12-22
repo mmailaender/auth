faa8928b16acd9005d80db4e330b81f6fdeb6b3f
Micha Mailaender <micha@mailaender.me>
2024-12-22 12:55:14 +0100

    Add reoon email verifier

--- src/routes/api/auth/webauthn/check-email/+server.ts
+++ src/routes/api/auth/webauthn/check-email/+server.ts
@@ -1,5 +1,6 @@
 import { createRegistration, verifyUserExists } from '$lib/auth/user';
 import type { RequestEvent, RequestHandler } from './$types';
+import { REOON_EMAIL_VERIFIER_TOKEN } from '$env/static/private';
 
 export const POST: RequestHandler = async (event: RequestEvent) => {
 	const data = await event.request.json();
@@ -10,9 +11,15 @@ export const POST: RequestHandler = async (event: RequestEvent) => {
 
 	const exists = await verifyUserExists(email);
 
-	if(exists == false) {
-		// TODO: Add code to validate email through reoon
+	if (exists == false) {
+		const response = await fetch(
+			`https://emailverifier.reoon.com/api/v1/verify?email=${email}&key=${REOON_EMAIL_VERIFIER_TOKEN}&mode=quick`
+		);
+		const verificationResult = await response.json();
 
+		if (verificationResult.status !== 'valid') {
+			return new Response('Invalid email', { status: 400 });
+		}
 
 		const otp = await createRegistration(email);
 
