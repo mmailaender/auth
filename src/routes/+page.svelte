fbdabbf61e57e0a27894ee6b17bbd144ca3d91b5
Micha Mailaender <micha@mailaender.me>
2024-12-22 21:41:00 +0100

    Add sign out

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,35 @@
 <script lang="ts">
-	import  { type User, signOut } from '$lib/auth/user';
+	import { type User } from '$lib/auth/user';
 	import type { PageData } from './$types';
+	import { goto } from '$app/navigation';
 
 	let { data }: { data: PageData } = $props();
 
-	let user: User = data.user ? JSON.parse(data.user) : null;
+	let user: User | null = $state(data.user ? JSON.parse(data.user) : null);
+
+	async function handleSignOut() {
+		try {
+			const response = await fetch('/api/auth/sign-out');
+			if (response.ok) {
+				user = null;
+				// if (redirectUrl) {
+				// 	goto(redirectUrl);
+				// }
+			} else {
+				console.error('Sign-out failed');
+			}
+		} catch (error) {
+			console.error('Error during sign-out:', error);
+		}
+	}
 </script>
 
-<div class="flex flex-col min-h-screen items-center justify-center gap-6">
+<div class="flex min-h-screen flex-col items-center justify-center gap-6">
 	{#if user}
 		Hello {user.email}
-		<a class="btn preset-filled-primary-500" href="/sign-out">Sign out</a>
+		<button class="btn preset-filled-primary-500" onclick={handleSignOut}>Sign out</button>
 	{:else}
 		Secret things ahead!
-        <a class="btn preset-filled-primary-500" href="/sign-in">Sign in</a>
+		<a class="btn preset-filled-primary-500" href="/sign-in">Sign in</a>
 	{/if}
 </div>
