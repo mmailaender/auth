12482aba0f493e0ec6b6d59f4d94d2585844d1dc
Micha Mailaender <micha@mailaender.me>
2025-01-02 12:54:22 +0100

    Removed mkcert

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { paraglide } from '@inlang/paraglide-sveltekit/vite';
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
-import fs from 'fs';
 
 export default defineConfig({
 	plugins: [
@@ -11,13 +10,6 @@ export default defineConfig({
 			outdir: './src/lib/paraglide'
 		})
 	],
-	server: {
-        https: {
-            key: fs.readFileSync(`${__dirname}/cert/key.pem`),
-            cert: fs.readFileSync(`${__dirname}/cert/cert.pem`)
-        },
-        proxy: {}
-    },
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
