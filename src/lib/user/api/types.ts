0b53fd6b0c9c394fa73e10ab84b6a394c5722447
Micha Mailaender <micha@mailaender.me>
2025-02-24 12:22:40 +0100

    Update profileData type

--- src/lib/user/api/types.ts
+++ src/lib/user/api/types.ts
@@ -1,30 +1,30 @@
-import { type } from "arktype";
+import { type } from 'arktype';
 
 export const profileData = type({
 	firstName: 'string',
 	lastName: 'string',
-	'avatar?': 'string'
+	'avatar?': 'string | undefined'
 });
-export type ProfileData = typeof profileData.infer
+export type ProfileData = typeof profileData.infer;
 
 export const verifyEmailData = type({
-    email: 'string',
-    userId: 'string'
-})
-export type VerifyEmailData = typeof verifyEmailData.infer
+	email: 'string',
+	userId: 'string'
+});
+export type VerifyEmailData = typeof verifyEmailData.infer;
 
 export const addEmailData = type({
-    email: 'string',
-    verificationOTP: 'string'
-})
-export type AddEmailData = typeof addEmailData.infer
+	email: 'string',
+	verificationOTP: 'string'
+});
+export type AddEmailData = typeof addEmailData.infer;
 
 export const createPasskeyAccountData = type({
-    userId: 'string',
-    encodedAttestationObject: 'string',
-    encodedClientDataJSON: 'string'
-})
-export type CreatePasskeyAccountData = typeof createPasskeyAccountData.infer
+	userId: 'string',
+	encodedAttestationObject: 'string',
+	encodedClientDataJSON: 'string'
+});
+export type CreatePasskeyAccountData = typeof createPasskeyAccountData.infer;
 
-export const cancelEmailVerificationData = type('string')
-export type CancelEmailVerificationData = typeof cancelEmailVerificationData.infer
+export const cancelEmailVerificationData = type('string');
+export type CancelEmailVerificationData = typeof cancelEmailVerificationData.infer;
