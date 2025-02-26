769c75c47ba67922a76e3537392cc40f6e810d43
Micha Mailaender <micha@mailaender.me>
2025-02-26 09:02:02 +0100

    Formatted UserProfile

--- src/lib/user/ui/page/UserProfile.svelte
+++ src/lib/user/ui/page/UserProfile.svelte
@@ -7,24 +7,24 @@
 	import VerificationFlow from '$lib/email/ui/widget/EmailVerification.svelte';
 	import ConnectedAccounts from '$lib/account/ui/widget/ConnectedAccounts.svelte';
 	import DeleteUser from '$lib/user/ui/widget/DeleteUser.svelte';
-    
+
 	import type { User } from '$lib/db/schema/types/custom';
 
 	let user: User = $state(JSON.parse(page.data.user));
 </script>
 
 <div
-	class="mx-auto flex w-full max-w-4xl flex-col rounded-container shadow-lg bg-surface-100-900 md:flex-row"
+	class="rounded-container bg-surface-100-900 mx-auto flex w-full max-w-4xl flex-col shadow-lg md:flex-row"
 >
 	<Sidebar currentSection="Profile" />
 	<div class="w-full p-6 md:w-3/4">
-		<h2 class="mb-6 text-lg font-bold text-surface-800-200">Profile details</h2>
+		<h2 class="text-surface-800-200 mb-6 text-lg font-bold">Profile details</h2>
 		<ProfileInfo bind:user />
 		<EmailAddresses bind:user />
 		<VerificationFlow bind:user />
 		<ConnectedAccounts bind:user />
-        <div class="flex justify-end">
-            <DeleteUser userId={user.id} />
-        </div>
+		<div class="flex justify-end">
+			<DeleteUser userId={user.id} />
+		</div>
 	</div>
 </div>
