57dc2daec4580e9b3b495f7e290f918059dcef94
Micha Mailaender <micha@mailaender.me>
2025-05-21 10:43:05 +0200

    style: simplify dialog close button styles by removing ring and state styles

--- nextjs/src/components/primitives/ui/dialog.tsx
+++ nextjs/src/components/primitives/ui/dialog.tsx
@@ -23,7 +23,7 @@ function DialogClose({ ...props }: React.ComponentProps<typeof DialogPrimitive.C
 		<DialogPrimitive.Close
 			data-slot="dialog-close"
 			{...props}
-			className="ring-offset-background focus:ring-ring data-[state=open]:bg-primary-500 data-[state=open]:text-surface-700-300 hover:bg-surface-300-700 absolute top-5 right-4 rounded-lg p-2 opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4"
+			className="hover:bg-surface-300-700 absolute top-5 right-4 rounded-lg p-2 opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4"
 		>
 			<XIcon />
 			<span className="sr-only">Close</span>
