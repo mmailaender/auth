248f59eeed0f82d3bb1042b1207b94d68d000504
Micha Mailaender <micha@mailaender.me>
2025-07-31 11:39:40 +0200

    refactor: remove unused code

--- nextjs/src/middleware.ts
+++ nextjs/src/middleware.ts
@@ -1,16 +1,12 @@
-import { fetchMutation, fetchQuery } from 'convex/nextjs';
 import { NextRequest, NextResponse } from 'next/server';
 import { getSessionCookie } from 'better-auth/cookies';
 import { createRouteMatcher } from '@/components/primitives/utils/routeMatcher';
-import { createAuth } from './components/auth/lib/auth';
 
 /* --------------------------------------------------------- */
 /* -------------------- route match helpers ---------------- */
 /* --------------------------------------------------------- */
 
 const isLogin = createRouteMatcher(['/signin']);
-const isInvitationAccept = createRouteMatcher(['/api/invitations/accept']);
-const isCreateOrg = createRouteMatcher(['/org/create']);
 const isPublic = createRouteMatcher([
 	'/',
 	'/signin',
@@ -35,47 +31,6 @@ const withRedirect = (to: string, request: NextRequest) => {
 /* --------------------------------------------------------- */
 /* ---------------------- main handler --------------------- */
 /* --------------------------------------------------------- */
-
-// export default convexAuthNextjsMiddleware(async (request, { convexAuth }) => {
-// 	const isAuthenticated = await convexAuth.isAuthenticated();
-
-// /* ---------- 3. Handle special API routes ---------- */
-// if (isInvitationAccept(request)) {
-// 	const url = new URL(request.url);
-// 	const invitationId = url.searchParams.get('invitationId');
-// 	if (invitationId) {
-// 		try {
-// 			await fetchMutation(
-// 				api.organizations.invitations.mutations.acceptInvitation,
-// 				{ invitationId },
-// 				{ token: await convexAuthNextjsToken() }
-// 			);
-// 		} catch (err) {
-// 			console.error('Error accepting invitation', err);
-// 		}
-// 	}
-// 	return NextResponse.redirect(new URL('/', request.url));
-// }
-
-// /* ---------- 5. Authenticated user checks ---------- */
-// const activeOrganization = await fetchQuery(
-// 	api.organizations.queries.getActiveOrganization,
-// 	{},
-// 	{ token: await convexAuthNextjsToken() }
-// );
-
-// // Allow access to org creation page even without active org
-// if (isCreateOrg(request)) {
-// 	return NextResponse.next();
-// }
-
-// // For all other protected routes, ensure user has an active organization
-// if (!activeOrganization) {
-// 	return NextResponse.redirect(new URL(withRedirect('/org/create', request), request.url));
-// }
-
-// 	return NextResponse.next();
-// });
 
 export async function middleware(request: NextRequest) {
 	const sessionCookie = getSessionCookie(request);
