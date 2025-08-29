f090b19c4401ab45b7da93a2c3e99edbf30b1993
Micha Mailaender <micha@mailaender.me>
2025-08-29 09:53:34 +0200

    style: standardize quote usage to single quotes across project files

--- svelte/src/lib/primitives/ui/drawer/drawer-trigger.svelte
+++ svelte/src/lib/primitives/ui/drawer/drawer-trigger.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-  import { Dialog as ArkDialog, type DialogTriggerProps } from '@ark-ui/svelte/dialog';
+	import { Dialog as ArkDialog, type DialogTriggerProps } from '@ark-ui/svelte/dialog';
 
-  let { ref = $bindable(null), ...restProps }: DialogTriggerProps = $props();
+	let { ref = $bindable(null), ...restProps }: DialogTriggerProps = $props();
 </script>
 
 <ArkDialog.Trigger bind:ref data-slot="drawer-trigger" {...restProps} />
