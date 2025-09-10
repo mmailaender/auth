8438549bd937e3bcf766bf8f747fa450210cf6c8
Micha Mailaender <micha@mailaender.me>
2025-09-10 14:34:38 +0200

    chore: add homepage URL and exclude page blocks from build config

--- svelte/src/convex/auth.constants.ts
+++ svelte/src/convex/auth.constants.ts
@@ -1,15 +1,27 @@
-export const AUTH_CONSTANTS = {
+export const AUTH_CONSTANTS: AuthConstants = {
 	providers: {
-		github: true,
-		google: false,
-		facebook: false,
-		keypass: false,
-		password: true,
-		emailOTP: true,
-		magicLink: true
+		password: true
 	},
 	validateEmails: true,
 	organizations: true,
 	terms: '#',
 	privacy: '#'
+};
+
+export type AuthConstants = {
+	providers: {
+		github?: boolean;
+		google?: boolean;
+		facebook?: boolean;
+		apple?: boolean;
+		microsoft?: boolean;
+		keypass?: boolean;
+		password?: boolean;
+		emailOTP?: boolean;
+		magicLink?: boolean;
+	};
+	organizations?: boolean;
+	terms?: string;
+	privacy?: string;
+	validateEmails?: boolean;
 };
