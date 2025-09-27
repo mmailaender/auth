05628b95ca6b5cda4ac9c8089881c254d7b55a2c
Micha Mailaender <micha@mailaender.me>
2025-09-27 12:29:56 +0200

    test google oauth

--- svelte/src/convex/auth.constants.ts
+++ svelte/src/convex/auth.constants.ts
@@ -2,6 +2,11 @@ import { AuthConstants } from './auth.constants.types';
 
 export const AUTH_CONSTANTS: AuthConstants = {
 	providers: {
+		github: true,
+		google: true,
 		password: true
-	}
+	},
+	organizations: true,
+	sendEmails: true,
+	validateEmails: true
 };
